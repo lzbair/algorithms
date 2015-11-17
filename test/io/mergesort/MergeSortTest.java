5c9c40a662750c6c1fd172b19844e2745ea76ded
Lahcen Zbair <nnzbairla@nestle.com>
2015-11-17 09:46:03 +0000

    More lambdas

--- test/io/mergesort/MergeSortTest.java
+++ test/io/mergesort/MergeSortTest.java
@@ -2,13 +2,12 @@ package io.mergesort;
 
 import static java.lang.Integer.parseInt;
 import static org.junit.Assert.assertTrue;
-import java.io.IOException;
+import static java.util.Arrays.asList;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 public class MergeSortTest {
@@ -17,49 +16,31 @@ public class MergeSortTest {
 
 	@Test
 	public void testIfArraySorted() throws Exception {
-		List<String> entries = new BlocExecutor<List<String>>() {
-			@Override
-			public List<String> doExecution() throws IOException {
-				return Files.readAllLines(Paths.get(FILE));
-			}
-		}.execute("Load data");
 
-		long[] values = new BlocExecutor<long[]>() {
-			@Override
-			public long[] doExecution() {
-				return entries.stream().mapToLong(e -> parseInt(e)).toArray();
-			}
-		}.execute("Transform data");
+		List<String> entries = perform("Load data", () -> {
+			return Files.readAllLines(Paths.get(FILE));
+		});
 
-		long[] sortedArray = new BlocExecutor<long[]>() {
-			@Override
-			public long[] doExecution() {
-				return new MergeSort().sort(values);
-			}
-		}.execute("Sort data");
+		long[] values = perform("Transform data", () -> {
+			return entries.stream().mapToLong(e -> parseInt(e)).toArray();
+		});
 
-		new BlocExecutor<Void>() {
-			@Override
-			public Void doExecution() {
-				List<long[]> sortedList = Arrays.asList(sortedArray);
-				sortedList.stream().forEach(
-						e -> assertTrue(sortedList.indexOf(e) + 1 == e[0]));
-				return null;
-			}
-		}.execute("Test data");
+		long[] sortedArray = perform("Sort data", () -> {
+			return new MergeSort().sort(values);
+		});
+
+		perform("Test data",
+				() -> {
+					List<long[]> sortedList = asList(sortedArray);
+					sortedList.stream().forEach(
+							e -> assertTrue(sortedList.indexOf(e) + 1 == e[0]));
+					return null;
+				});
 	}
 
-	interface BlocExecutor<T> {
-		T doExecution() throws Exception;
-
-		default T execute(String operation) throws Exception {
-			long start = System.currentTimeMillis();
-			T result = doExecution();
-			long end = System.currentTimeMillis();
-			System.out.println(operation + " : " + ((end - start) / 1000) + " Seconds");
-			return result;
-		}
-
+	private <T> T perform(String operation, BlocExecutor<T> blocExecutor)
+			throws Exception {
+		return blocExecutor.execute(operation);
 	}
 
 }
