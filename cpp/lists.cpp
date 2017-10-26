c7a4096e694bbc8913a3424e3799f061317c99c8
Zbair Lahcen <Lahcen.Zbair@nespresso.com>
2017-10-26 17:17:35 +0100

    list.cpp

--- cpp/lists.cpp
+++ cpp/lists.cpp
@@ -1,31 +1,36 @@
+#include <stdio.h>
 #include <stdlib.h>
 struct LinkedList {
   void* head;
   LinkedList* tail;
 };
 
-LinkedList prepend(void* head, LinkedList* tail){
-    LinkedList list = {head, tail};
+LinkedList* prepend(void* head, LinkedList* tail){
+	LinkedList* list = (LinkedList *)malloc(sizeof(LinkedList));
+    *list = {head, tail};
     return list;
 }
 
 void* get(int index, LinkedList* list){
-    LinkedList* current = (LinkedList *)malloc(sizeof(LinkedList));
-    current = list;
+    LinkedList* current = list;
     int i;
     for(i = 0; i < index; i++){
         current = (*list).tail;
     }
-    void* result = (*current).head;
-    free(current);
-    return result;
+    return (*current).head;
 }
 
-int head = 9;
-int next = 5;
-LinkedList tail = prepend(&next, NULL);
-LinkedList list = prepend(&head, &tail);
-int* value = get(1, &list);
-printf("%d\n", *value);
+int main(){
+	int head = 9;
+	int next = 5;
+	LinkedList* tail = prepend(&next, NULL);
+	LinkedList* list = prepend(&head, tail);
+	int* value = (int*)get(0, list);
+	printf("%d\n", *value);
+	free(tail);
+	free(list);
+}
+
+
 
 
